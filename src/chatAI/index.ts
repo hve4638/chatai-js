e206521bb57e900163442b5292a63bbb13e61f0b
Hve <hve4638@gmail.com>
2024-12-03 21:14:12 +0900

    FIX : 디렉토리 이동

---
+++ src/chatAI/index.ts
@@ -0,0 +1,12 @@
+export { Models, ModelCategory } from './models'
+export type {
+    RequestForm,
+    RequestOption,
+} from './types/request-form'
+export {
+    ChatRole,
+    ChatType
+} from './types/request-form'
+
+import { default as AIModelAPI } from './AIModelAPI'
+export default AIModelAPI;
